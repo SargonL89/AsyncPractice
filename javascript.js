062fd2d7f8465781cab5cbb9cbb05984e71e37f7
Leandro Magnotti <leandro.magnotti@gmail.com>
2023-07-07 23:18:32 -0300

    A SweetAlert was added

--- javascript.js
+++ javascript.js
@@ -16,7 +16,31 @@ function consultar() {
     });
 }
 
-consultar();
+function consultar2() {
+    const btn2 = document.getElementById('btn2');
+
+    btn2.addEventListener('click', () => {
+        const input = document.getElementById('input1').value;
+        Swal.fire({
+            title: "Estamos buscando coincidencias en la base de datos que no tenemos...",
+            color: "lightblue",
+            showConfirmButton: false,
+            imageUrl: "/images/blue-black-load.gif",
+            imageWidth: 400,
+            imageAlt: "Custom image",
+            background: "black",
+            timer: 3000,
+        });
+        const filtrados = baseDatosFicticia.filter((placa) => {
+            return (placa.fabricante.toLocaleLowerCase() === input.toLocaleLowerCase() ||
+            placa.marca.toLocaleLowerCase() === input.toLocaleLowerCase() ||
+            placa.modelo.toLocaleLowerCase() === input.toLocaleLowerCase() ||
+            placa.memoria.toLocaleLowerCase() === input.toLocaleLowerCase() ||
+            placa.precio === input)
+        });
+        mostrarResultados(filtrados)
+    });
+}
 
 function mostrarResultados(x) {
     const tableBody = document.getElementById('tableBody')
@@ -32,3 +56,6 @@ function mostrarResultados(x) {
             </tr>`;
     })
 }
+
+consultar();
+consultar2();
