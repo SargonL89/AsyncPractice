c4d779793d9a0fa0f6b19c650ba9bfdbf0aa8ede
Leandro Magnotti <leandro.magnotti@gmail.com>
2023-07-14 07:22:41 -0300

    Async Function for MostrarResultados was added

--- javascript.js
+++ javascript.js
@@ -2,6 +2,7 @@ function consultar() {
     const btn1 = document.getElementById('btn1');
 
     btn1.addEventListener('click', () => {
+
         const input = document.getElementById('input1').value;
         setTimeout(() => {
             const filtrados = baseDatosFicticia.filter((placa) => {
@@ -42,20 +43,49 @@ function consultar2() {
     });
 }
 
+async function consultar3() {
+    try {
+        const btn3 = document.getElementById('btn3');
+        btn3.addEventListener('click', async () => {
+            const respuesta = await buscarElementos();
+            return mostrarResultados(respuesta);
+        });
+
+    } catch (error) {
+        console.error('Error al consultar la base de datos:', error);
+        throw new Error('No se pudieron obtener los datos de la base de datos');
+    }
+}
+
+function buscarElementos() {
+    const input = document.getElementById('input1').value;
+
+    const filtrados = baseDatosFicticia.filter((placa) => {
+        return (placa.fabricante.toLocaleLowerCase() === input.toLocaleLowerCase() ||
+        placa.marca.toLocaleLowerCase() === input.toLocaleLowerCase() ||
+        placa.modelo.toLocaleLowerCase() === input.toLocaleLowerCase() ||
+        placa.memoria.toLocaleLowerCase() === input.toLocaleLowerCase() ||
+        placa.precio === input)
+    });
+    return filtrados;
+}
+
+
 function mostrarResultados(x) {
     const tableBody = document.getElementById('tableBody')
 
-    x.forEach((placa) => {
-        tableBody.innerHTML += `<tr>
-            <td>${placa.id}</td>
-            <td>${placa.fabricante}</td>
-            <td>${placa.marca}</td>
-            <td>${placa.modelo}</td>
-            <td>${placa.memoria}</td>
-            <td>${placa.precio}</td>
-            </tr>`;
-    })
+        x.forEach((placa) => {
+            tableBody.innerHTML += `<tr>
+                <td>${placa.id}</td>
+                <td>${placa.fabricante}</td>
+                <td>${placa.marca}</td>
+                <td>${placa.modelo}</td>
+                <td>${placa.memoria}</td>
+                <td>${placa.precio}</td>
+                </tr>`;
+        })
 }
 
 consultar();
 consultar2();
+consultar3();
