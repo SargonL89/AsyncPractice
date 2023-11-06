5b66e19cf38e58827b5a627e5008410bef5f0861
Leandro Magnotti <leandro.magnotti@gmail.com>
2023-11-06 20:41:29 -0300

    base datos modificada

--- baseDatos.js
+++ baseDatos.js
@@ -5,7 +5,8 @@ const baseDatosFicticia = [
       marca: "Palit",
       modelo: "RTX 2060 SUPER",
       memoria: "8 GB",
-      precio: "$246999",
+      precio: 270,
+      img: "https://res.cloudinary.com/sargonl89/image/upload/v1699311294/placa01_b7gz9f.jpg"
     },
     {
       id: "002",
@@ -13,7 +14,8 @@ const baseDatosFicticia = [
       marca: "Nvidia",
       modelo: "RTX 3070",
       memoria: "8 GB",
-      precio: "$350999",
+      precio: 383,
+      img: "https://res.cloudinary.com/sargonl89/image/upload/v1699311300/placa02_aeabcz.jpg"
     },
     {
       id: "003",
@@ -21,7 +23,8 @@ const baseDatosFicticia = [
       marca: "MSI",
       modelo: "RTX 3080",
       memoria: "10 GB",
-      precio: "$624999",
+      precio: 683,
+      img: "https://res.cloudinary.com/sargonl89/image/upload/v1699311306/placa03_ym3kax.jpg"
     },
     {
       id: "004",
@@ -29,7 +32,8 @@ const baseDatosFicticia = [
       marca: "Nvidia",
       modelo: "RTX 3090",
       memoria: "24 GB",
-      precio: "$739999",
+      precio: 808,
+      img: "https://res.cloudinary.com/sargonl89/image/upload/v1699311311/placa04_fmxmko.jpg"
     },
     {
       id: "005",
@@ -37,7 +41,8 @@ const baseDatosFicticia = [
       marca: "Nvidia",
       modelo: "RTX 3090",
       memoria: "24 GB",
-      precio: "$864749",
+      precio: 945,
+      img: "https://res.cloudinary.com/sargonl89/image/upload/v1699311315/placa05_mdze3v.jpg"
     },
     {
       id: "006",
@@ -45,7 +50,8 @@ const baseDatosFicticia = [
       marca: "Asus",
       modelo: "RTX 4070",
       memoria: "12 GB",
-      precio: "$755959",
+      precio: 826,
+      img: "https://res.cloudinary.com/sargonl89/image/upload/v1699311319/placa06_yq2lta.jpg"
     },
     {
       id: "007",
@@ -53,7 +59,8 @@ const baseDatosFicticia = [
       marca: "MSI",
       modelo: "RTX 4080",
       memoria: "16 GB",
-      precio: "$1055647",
+      precio: 1153,
+      img: "https://res.cloudinary.com/sargonl89/image/upload/v1699311331/placa07_ua7t9j.jpg"
     },
     {
       id: "008",
@@ -61,7 +68,8 @@ const baseDatosFicticia = [
       marca: "AMD",
       modelo: "RX 580",
       memoria: "8 GB",
-      precio: "$232999",
+      precio: 254,
+      img: "https://res.cloudinary.com/sargonl89/image/upload/v1699311332/placa08_t36vjv.jpg"
     },
     {
       id: "009",
@@ -69,7 +77,8 @@ const baseDatosFicticia = [
       marca: "PowerColor",
       modelo: "RX 6500 XT",
       memoria: "4 GB",
-      precio: "$169423",
+      precio: 185,
+      img: "https://res.cloudinary.com/sargonl89/image/upload/v1699311333/placa09_zyhbxj.jpg"
     },
     {
       id: "010",
@@ -77,7 +86,8 @@ const baseDatosFicticia = [
       marca: "Sapphire",
       modelo: "RX 6700 XT",
       memoria: "12 GB",
-      precio: "$269999",
+      precio: 295,
+      img: "https://res.cloudinary.com/sargonl89/image/upload/v1699311333/placa10_fycirf.jpg"
     },
     {
       id: "011",
@@ -85,7 +95,8 @@ const baseDatosFicticia = [
       marca: "ASRock",
       modelo: "RX 6800 XT",
       memoria: "16 GB",
-      precio: "$574999",
+      precio: 628,
+      img: "https://res.cloudinary.com/sargonl89/image/upload/v1699311366/placa11_zplp8z.jpg"
     },
     {
       id: "012",
@@ -93,7 +104,8 @@ const baseDatosFicticia = [
       marca: "ASRock",
       modelo: "RX 6900 XT",
       memoria: "16 GB",
-      precio: "$407019",
+      precio: 444,
+      img: "https://res.cloudinary.com/sargonl89/image/upload/v1699311366/placa12_s0wfbz.jpg"
     },
     {
       id: "013",
@@ -101,7 +113,8 @@ const baseDatosFicticia = [
       marca: "ASRock",
       modelo: "RX 6900 XT",
       memoria: "16 GB",
-      precio: "$440889",
+      precio: 481,
+      img: "https://res.cloudinary.com/sargonl89/image/upload/v1699311367/placa13_jgvzmi.jpg"
     },
     {
       id: "014",
@@ -109,7 +122,8 @@ const baseDatosFicticia = [
       marca: "Asus",
       modelo: "ROG-STRIX-RTX4080-O16G-GAMING",
       memoria: "16 GB",
-      precio: "$1758102",
+      precio: 1921,
+      img: "https://res.cloudinary.com/sargonl89/image/upload/v1699311368/placa14_wowauf.jpg"
     },
     {
       id: "015",
@@ -117,6 +131,7 @@ const baseDatosFicticia = [
       marca: "Asus",
       modelo: "ROG-STRIX-RTX4080-O16G-GAMING",
       memoria: "16 GB",
-      precio: "$2296302",
+      precio: 2509,
+      img: "https://res.cloudinary.com/sargonl89/image/upload/v1699311365/placa15_hvnokv.jpg"
     },
   ];
